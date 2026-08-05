d06eee65ac4cef09c524764d6855f49d7a55a1a6
Georgii Zhabieiev <zhabeev@gmail.com>
2026-08-05 23:07:52 +0200

    change the project

---
+++ regression-jhipster/src/main/java/com/aqa/jhipster/api/services/ApiService.java
@@ -0,0 +1,13 @@
+package com.aqa.jhipster.api.services;
+
+import com.aqa.core.services.GeneralApiService;
+import jakarta.ws.rs.core.UriBuilder;
+
+import static com.aqa.jhipster.api.enumeration.Property.URL_API;
+
+public abstract class ApiService extends GeneralApiService {
+
+    protected ApiService() {
+        super(UriBuilder.fromUri(URL_API.read()).toTemplate());
+    }
+}
--- regression-toolshop/src/main/java/com/aqa/toolshop/api/enumeration/Property.java
+++
@@ -1,9 +0,0 @@
-package com.aqa.toolshop.api.enumeration;
-
-import com.aqa.core.enumerations.PropertyReader;
-
-public enum Property implements PropertyReader {
-    URL_API,
-    USER_EMAIL,
-    USER_PASSWORD;
-}

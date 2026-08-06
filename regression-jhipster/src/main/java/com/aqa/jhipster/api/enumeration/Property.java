94215206658fbe4ef3e65f92eb4f712f9cdd98ca
Georgii Zhabieiev <zhabeev@gmail.com>
2026-08-06 12:19:43 +0200

    Update: properties

--- regression-jhipster/src/main/java/com/aqa/jhipster/api/enumeration/Property.java
+++ regression-jhipster/src/main/java/com/aqa/jhipster/api/enumeration/Property.java
@@ -3,7 +3,5 @@ package com.aqa.jhipster.api.enumeration;
 import com.aqa.core.enumerations.PropertyReader;
 
 public enum Property implements PropertyReader {
-    URL_API,
-    USER_EMAIL,
-    USER_PASSWORD;
+    URL_API
 }
